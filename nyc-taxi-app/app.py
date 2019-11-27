7398d05eeb7330e05fd790b4d60a8249edab2ad7
Ezhil Nithya Suresh <nithya@Ezhils-MacBook-Air.local>
2019-11-27 21:13:56 +0530

    Added reading dropped rides info from redis

--- nyc-taxi-app/app.py
+++ nyc-taxi-app/app.py
@@ -4,15 +4,29 @@ import redis
 import logging
 
 from   flask import Flask
+from   flask import request
 
-print ("Starting ...")
-calls = 0
 
 app = Flask(__name__)
 
+# get redis hostname and port number
+redis_host = 'localhost'
+if 'REDIS_HOST' in os.environ:
+	redis_host = os.environ['REDIS_HOST']
 
-redis_host = os.environ['REDIS_HOST']
-redis_port = int(os.environ['REDIS_PORT'])
+redis_port = 6379
+if 'REDIS_PORT' in os.environ:
+	redis_port = int(os.environ['REDIS_PORT'])
+
+# get debug setting
+debug_enabled = False
+if 'NYC_TAXI_APP_DEBUG' in os.environ:
+	debug_enabled = bool(int(os.environ['NYC_TAXI_APP_DEBUG']))
+
+print ("Starting ...Redis Host[{}] Redis Port[{}] Debug[{}] ".format(redis_host, redis_port, debug_enabled))
+
+def is_debug_enabled ():
+	return debug_enabled
 
 print ('Connecting redis with {}:{}'.format(redis_host, redis_port))
 cache = redis.Redis(host=redis_host, port=redis_port)
@@ -21,8 +35,25 @@ cache = redis.Redis(host=redis_host, port=redis_port)
 
 @app.route('/')
 def hello():
-	calls = cache.incr('visited')
-	return 'Hello there {}\n'.format(calls)
+
+	# get the completed rides from cache
+	rides   = cache.zrange('snapshot', 0, -1)
+
+	# return number of rides. If debug enabled return all data
+	ret_str = '{}\n'.format(len(rides))
+
+	return ret_str
+
+
+@app.route('/debug')
+def debug():
+
+	# return number of rides. If debug enabled return all data
+	ret_str = hello ()
+	for rec in rides:
+		ret_str = ret_str + str(rec) + '\n'
+
+	return ret_str
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
