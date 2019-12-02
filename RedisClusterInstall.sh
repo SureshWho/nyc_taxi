9a7059fbe8a8950249bc18d61f9a813ee1d23413
Ezhil Nithya Suresh <nithya@Ezhils-MacBook-Air.local>
2019-12-02 09:04:36 +0530

    Fixed a wrong path to yaml

--- RedisClusterInstall.sh
+++ RedisClusterInstall.sh
@@ -3,23 +3,29 @@
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 chmod 700 get_helm.sh
 ./get_helm.sh
+sleep 15
 
 ### Install has some bug we need to delete and re-create ###
 kubectl get all --all-namespaces | grep tiller
 kubectl delete deployment tiller-deploy -n kube-system
 kubectl delete service tiller-deploy -n kube-system
 kubectl get all --all-namespaces | grep tiller
+sleep 15
 
 helm init
+sleep 15
 
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+sleep 15
 
 kubectl --namespace kube-system get pods | grep tiller
 
 echo "Waiting till tiller gets ready"
-sleep 10
+sleep 20
+kubectl --namespace kube-system get pods | grep tiller
+sleep 5
 
 sudo mkdir /mnt/data/
 #sudo mkdir /mnt/data/0
@@ -40,7 +46,7 @@ kubectl apply -f deployments/volumn_3.yml
 
 
 helm repo update
-helm install stable/redis --name nyc-taxi --values values-production.yaml
+helm install stable/redis --name nyc-taxi --values deployments/values-production.yaml
 #helm install stable/redis
 
 
