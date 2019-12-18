ae87b16d7eea1c7934da889083f3ee6ee8d2631a
SureshWho <nithya@Ezhils-MacBook-Air.local>
2019-12-18 11:17:49 +0530

    Added wait states and msgs

--- HelmInstall.sh
+++ HelmInstall.sh
@@ -4,22 +4,26 @@ echo "Installing Helm"
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 chmod 700 get_helm.sh
 ./get_helm.sh
-sleep 15
+echo "Wait..."
+sleep 10
 
 ### Install has some bug we need to delete and re-create ###
+echo "Cleaningup tiller, Ignore any error(s). Might take upto 3m0s"
 kubectl get all --all-namespaces | grep tiller
 kubectl delete deployment tiller-deploy -n kube-system
 kubectl delete service tiller-deploy -n kube-system
 kubectl get all --all-namespaces | grep tiller
-sleep 15
+sleep 5
 
 helm init
+echo "Wait...Re-initializing tiller"
 sleep 15
 
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-sleep 15
+echo "Wait...Creating service accounts"
+sleep 10
 
 kubectl --namespace kube-system get pods | grep tiller
 
@@ -27,5 +31,4 @@ echo "Waiting till tiller gets ready"
 sleep 20
 kubectl --namespace kube-system get pods | grep tiller
 sleep 5
-
 
