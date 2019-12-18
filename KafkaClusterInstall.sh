efd7a730e29380bcd827d2e3f227d18502b57f62
SureshWho <nithya@Ezhils-MacBook-Air.local>
2019-12-18 20:51:28 +0530

    Added 3 replications

--- KafkaClusterInstall.sh
+++ KafkaClusterInstall.sh
@@ -1,15 +1,15 @@
 
 sudo mkdir /mnt/data/
-#sudo mkdir /mnt/data/0
-#sudo mkdir /mnt/data/1
-#sudo mkdir /mnt/data/2
-#sudo mkdir /mnt/data/3
+sudo mkdir /mnt/data/0
+sudo mkdir /mnt/data/1
+sudo mkdir /mnt/data/2
+sudo mkdir /mnt/data/3
 
 sudo sudo chown -R 1001:1001 /mnt/data/
-#sudo chown -R 1001:1001 /mnt/data/0
-#sudo chown -R 1001:1001 /mnt/data/1
-#sudo chown -R 1001:1001 /mnt/data/2
-#sudo chown -R 1001:1001 /mnt/data/3
+sudo chown -R 1001:1001 /mnt/data/0/
+sudo chown -R 1001:1001 /mnt/data/1/
+sudo chown -R 1001:1001 /mnt/data/2/
+sudo chown -R 1001:1001 /mnt/data/3/
 
 kubectl apply -f deployments/volumn_0.yml 
 kubectl apply -f deployments/volumn_1.yml
@@ -19,6 +19,6 @@ kubectl apply -f deployments/volumn_3.yml
 echo "Settingup kafka cluster"
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm install bitnami/kafka --name nyc-taxi-kafka -f deployments/values-kafka.yml
+helm install bitnami/kafka --name nyc-taxi-kafka -f deployments/values-production-kafka.yml
 
 
